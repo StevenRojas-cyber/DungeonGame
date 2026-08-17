e594f55be5afa94c05b29ad10aae8d612cff6a37
StevenRojas-cyber <stevenisaac3103@gmail.com>
2026-08-17 00:10:07 -0300

    Probabilidad de destruccion

--- Assets/Objetos/Interactuables/Cofre/Scripts/Chest.cs
+++ Assets/Objetos/Interactuables/Cofre/Scripts/Chest.cs
@@ -17,6 +17,10 @@ public class Chest : MonoBehaviour, IInteractable, IDestructible
     [Header("Drop Settings")]
     [SerializeField] private float scatterRange = 0.5f;
 
+    [Header("Risk Settings")]
+    [Tooltip("Porcentaje de probabilidad (0 a 100) de perder CADA ítem si se abre a la fuerza.")]
+    [SerializeField][Range(0f, 100f)] private float DestroyRiskPercentage = 40f;
+
 
 
     public void Interact(GameObject Interactor)
@@ -27,7 +31,7 @@ public class Chest : MonoBehaviour, IInteractable, IDestructible
 
         if (NeedKey == false)
         {
-            OpenChest();
+            OpenChest(false);
             return;
         }
 
@@ -35,7 +39,7 @@ public class Chest : MonoBehaviour, IInteractable, IDestructible
         {
             Interactor.GetComponent<PlayerAttributes>().UseKey();
 
-            OpenChest();
+            OpenChest(false);
         }
         else
         {
@@ -43,10 +47,23 @@ public class Chest : MonoBehaviour, IInteractable, IDestructible
         }
     }
 
-    private void OpenChest()
+    private void OpenChest(bool isForcedOpen)
     {
         for (int i = 0; i < ItemsCount; i++)
         {
+            // Si fue forzado, calculamos la probabilidad de destrucción por cada ítem
+            if (isForcedOpen)
+            {
+                float roll = Random.Range(0f, 100f);
+
+                // Si el número aleatorio cae dentro del riesgo, el ítem se destruye
+                if (roll <= DestroyRiskPercentage)
+                {
+                    Debug.Log("¡Un ítem fue destruido por la fuerza del impacto!");
+                    continue; // El comando 'continue' salta esta iteración y pasa a la siguiente, evitando el Instantiate
+                }
+            }
+
             GameObject ItemToSpawn = GetRandomItem();
 
             Vector3 randomOffset = new Vector3(
@@ -74,7 +91,7 @@ public class Chest : MonoBehaviour, IInteractable, IDestructible
         if (Interactor.GetComponent<FireBall>().GetCurrentMagicLevel() >= MagicDefense)
         {
 
-            OpenChest();
+            OpenChest(true);
         }
         else
         {
@@ -88,7 +105,7 @@ public class Chest : MonoBehaviour, IInteractable, IDestructible
 
         if (Interactor.GetComponent<Attack>().GetCurrentMeleeLevel() >= MeleeDefense)
         {
-            OpenChest();
+            OpenChest(true);
         }
         else
         {
